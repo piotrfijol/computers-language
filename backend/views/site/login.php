a8df7366de44a702738ee610aed9a8951983d4ca
piotrfijol <piotr.fijol1@gmail.com>
2022-05-01 16:49:26 +0200

    Styling CMS login page

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -9,24 +9,31 @@ use yii\bootstrap4\Html;
 
 $this->title = 'Login';
 ?>
+
 <div class="site-login">
-    <div class="mt-5 offset-lg-3 col-lg-6">
-        <h1><?= Html::encode($this->title) ?></h1>
 
-        <p>Please fill out the following fields to login:</p>
+    <div class="d-flex justify-content-center align-items-center h-75 row pt-5 py-5">
+        <div class="col-xs-12 col-sm-8 m-auto form-container">
+            <h1 class="text-center form-title" >Panel kontrolny</h1>
+            <?php $form = ActiveForm::begin(['id' => 'login-form', 'options' => ['style' => 'padding-top: 25px;']]); ?>
 
-        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+            <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'placeholder' => 'Login'])->label('') ?>
 
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Hasło'])->label('') ?>
 
-            <?= $form->field($model, 'password')->passwordInput() ?>
+            <?= $form->field($model, 'rememberMe')->checkbox()->label("Zapamiętaj mnie!") ?>
 
-            <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
+            
             <div class="form-group">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
+                <?= Html::submitButton('Zaloguj', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
             </div>
 
-        <?php ActiveForm::end(); ?>
+            <div style="color:#999;margin:1em 0">
+                <?= Html::a('Nie pamiętam hasła', ['site/request-password-reset'], ['style' => 'display: block; text-align: center']) ?>
+            </div>
+
+
+            <?php ActiveForm::end(); ?>
+        </div>
     </div>
 </div>
