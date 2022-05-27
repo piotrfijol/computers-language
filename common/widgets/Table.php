86265f23731c4413bf4967e34dad06265c31328b
piotrfijol <piotr.fijol1@gmail.com>
2022-05-27 20:36:40 +0200

    Updated the dependency

---
+++ common/widgets/Table.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace common\widgets;
+
+use Yii;
+
+class Table extends \yii\base\Widget {
+
+    public function init() {
+
+    }
+
+    public function run() {
+        
+    }
+
+}
